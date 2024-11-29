391ce4ad8330bf3efa5ecca70892d51ad754dcda
ddomeke <durandomeke@gmail.com>
2024-11-29 22:26:28 +0300

    first compile

--- contracts/ArbitrumBridge.sol
+++ contracts/ArbitrumBridge.sol
@@ -1,86 +1,110 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MyNFT is ERC721URIStorage, OwnerIsCreator {
-    string constant TOKEN_URI =
-        "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
-    uint256 internal tokenId;
-
-    constructor() ERC721("MyNFT", "MNFT") {}
-
-    function mint(address to) public onlyOwner {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, TOKEN_URI);
-        unchecked {
-            tokenId++;
-        }
-    }
-}
-
-contract CCIPTokenAndDataReceiver is CCIPReceiver, OwnerIsCreator {
-    MyNFT public nft;
-    uint256 price;
-
-    mapping(uint64 => bool) public whitelistedSourceChains;
-    mapping(address => bool) public whitelistedSenders;
+contract ArbitrumBridge is CCIPReceiver, OwnerIsCreator {
+    IRouterClient router;
+    ERC20Burnable  public sourceTokenAddress;
 
     event MintCallSuccessfull();
+    event TokensBurned(
+        bytes32 indexed messageId, // The unique ID of the message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address receiver, // The address of the receiver on the destination chain.
+        address token, // The token address that was transferred.
+        uint256 tokenAmount, // The token amount that was transferred.
+        address feeToken, // the token address used to pay CCIP fees.
+        uint256 fees // The fees paid for sending the message.
+    );
 
-    error SourceChainNotWhitelisted(uint64 sourceChainSelector);
-    error SenderNotWhitelisted(address sender);
+    
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
 
-    modifier onlyWhitelistedSourceChain(uint64 _sourceChainSelector) {
-        if (!whitelistedSourceChains[_sourceChainSelector])
-            revert SourceChainNotWhitelisted(_sourceChainSelector);
-        _;
-    }
-
-    modifier onlyWhitelistedSenders(address _sender) {
-        if (!whitelistedSenders[_sender]) revert SenderNotWhitelisted(_sender);
-        _;
-    }
-
-    constructor(address router, uint256 _price) CCIPReceiver(router) {
-        nft = new MyNFT();
-        price = _price;
-    }
-
-    function whitelistSourceChain(
-        uint64 _sourceChainSelector
-    ) external onlyOwner {
-        whitelistedSourceChains[_sourceChainSelector] = true;
-    }
-
-    function denylistSourceChain(
-        uint64 _sourceChainSelector
-    ) external onlyOwner {
-        whitelistedSourceChains[_sourceChainSelector] = false;
-    }
-
-    function whitelistSender(address _sender) external onlyOwner {
-        whitelistedSenders[_sender] = true;
-    }
-
-    function denySender(address _sender) external onlyOwner {
-        whitelistedSenders[_sender] = false;
+    constructor(address _router, address _sourceTokenAddress) CCIPReceiver(_router) {
+        router = IRouterClient(_router);
+        sourceTokenAddress = ERC20Burnable(_sourceTokenAddress);
     }
 
     function _ccipReceive(
         Client.Any2EVMMessage memory message
-    ) 
-        internal
-        onlyWhitelistedSourceChain(message.sourceChainSelector)
-        onlyWhitelistedSenders(abi.decode(message.sender, (address))) 
-        override 
-    {
-        require(message.destTokenAmounts[0].amount >= price, "Nots enough CCIP-BnM for mint");
-        (bool success, ) = address(nft).call(message.data);
-        require(success);
+    ) internal override {
+        (bool success, ) = address(sourceTokenAddress).call(message.data);
+        require(success, "Token mint failed");
         emit MintCallSuccessfull();
+    }
+
+       // Token kilitleme fonksiyonu
+    function burnTokens(
+        uint64 _destinationChainSelector,
+        address _receiver,
+        address _token,
+        uint256 _amount
+    ) external onlyOwner returns (bytes32 messageId) {
+
+        require(_amount > 0, "Amount should be greater than 0");
+        
+        if (_amount > sourceTokenAddress.balanceOf(msg.sender))
+            revert NotEnoughBalance(sourceTokenAddress.balanceOf(msg.sender), _amount);
+
+        ERC20Burnable(sourceTokenAddress).burn(_amount);
+
+        Client.EVMTokenAmount[]
+            memory tokenAmounts = new Client.EVMTokenAmount[](1);
+        Client.EVMTokenAmount memory tokenAmount = Client.EVMTokenAmount({
+            token: _token,
+            amount: _amount
+        });
+        tokenAmounts[0] = tokenAmount;
+
+        // Build the CCIP Message
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver),
+            data: abi.encodeWithSignature(
+                "tranfer(address,address, uint256)",
+                _receiver,
+                msg.sender,
+                _amount
+            ),
+            tokenAmounts: tokenAmounts,
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV2({
+                    gasLimit: 0, // Gas limit for the callback on the destination chain
+                    allowOutOfOrderExecution: true // Allows the message to be executed out of order relative to other messages from the same sender
+                })
+            ),
+            feeToken: address(0)
+        });
+
+        // Get the fee required to send the message
+        uint256 fees = router.getFee(_destinationChainSelector, message);
+
+        if (fees > address(this).balance)
+            revert NotEnoughBalance(address(this).balance, fees);
+
+        // Approve Router to spend CCIP-BnM tokens we send
+        IERC20(_token).approve(address(router), _amount);
+
+        // Send CCIP Message
+        messageId = router.ccipSend(_destinationChainSelector, message);
+
+        emit TokensBurned(
+            messageId,
+            _destinationChainSelector,
+            _receiver,
+            _token,
+            _amount,
+            address(0),
+            fees
+        );
+    }
+
+    function setSourceAddress(address _sourceTokenAddress) external onlyOwner {
+        sourceTokenAddress = ERC20Burnable(_sourceTokenAddress);
     }
 }
--- contracts/MultichainControl.sol
+++ contracts/MultichainControl.sol
@@ -5,15 +5,17 @@ import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/O
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 
-contract MultichainControl is OwnerIsCreator {
+contract MultichainControl is CCIPReceiver, OwnerIsCreator {
     IRouterClient router;
-
-    mapping(uint64 => bool) public whitelistedChains;
+    IERC20 public sourceTokenAddress;
+    address private _owner; // Address of the contract owner
 
     error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
-    error DestinationChainNotWhitelisted(uint64 destinationChainSelector);
-    error NothingToWithdraw();
+
+    // Kullanıcıların kilitlediği token miktarlarını tutar
+    mapping(address => uint256) public lockedTokens;
 
     event TokensTransferred(
         bytes32 indexed messageId, // The unique ID of the message.
@@ -24,40 +26,48 @@ contract MultichainControl is OwnerIsCreator {
         address feeToken, // the token address used to pay CCIP fees.
         uint256 fees // The fees paid for sending the message.
     );
+    event TokensLocked(address indexed user, uint256 amount);
+    event TokensUnlocked(uint256 amount);
+    event MintCallSuccessfull();
 
-    modifier onlyWhitelistedChain(uint64 _destinationChainSelector) {
-        if (!whitelistedChains[_destinationChainSelector])
-            revert DestinationChainNotWhitelisted(_destinationChainSelector);
+    // Modifier to restrict the function to be executed only on Ethereum mainnet (chainId == 1)
+    modifier onlyEthereum() {
+        require(
+            block.chainid == 1,
+            "This function can only be executed on Ethereum Mainnet"
+        );
         _;
     }
 
-    constructor(address _router) {
+    constructor(address _router, address _sourceTokenAddress) CCIPReceiver(_router) {
+        _owner = msg.sender;
         router = IRouterClient(_router);
+        sourceTokenAddress = IERC20(_sourceTokenAddress);
     }
 
-    function whitelistChain(
-        uint64 _destinationChainSelector
-    ) external onlyOwner {
-        whitelistedChains[_destinationChainSelector] = true;
-    }
-
-    function denylistChain(
-        uint64 _destinationChainSelector
-    ) external onlyOwner {
-        whitelistedChains[_destinationChainSelector] = false;
-    }
-
-    function transferTokens(
+    // Token kilitleme fonksiyonu
+    function lockTokens(
         uint64 _destinationChainSelector,
         address _receiver,
         address _token,
         uint256 _amount
-    )
-        external
-        onlyOwner
-        onlyWhitelistedChain(_destinationChainSelector)
-        returns (bytes32 messageId)
-    {
+    ) external onlyOwner onlyEthereum returns (bytes32 messageId) {
+        require(_amount > 0, "Amount should be greater than 0");
+
+        if (_amount > sourceTokenAddress.balanceOf(msg.sender))
+            revert NotEnoughBalance(sourceTokenAddress.balanceOf(msg.sender), _amount);
+
+
+        lockedTokens[msg.sender] += _amount;
+
+        // Approve Router to spend CCIP-BnM tokens we send
+        IERC20(sourceTokenAddress).approve(address(this), _amount);
+
+        // Token'ları kontrata transfer et
+        IERC20(sourceTokenAddress).transferFrom(msg.sender, address(this), _amount);
+
+        emit TokensLocked(msg.sender, _amount);
+
         Client.EVMTokenAmount[]
             memory tokenAmounts = new Client.EVMTokenAmount[](1);
         Client.EVMTokenAmount memory tokenAmount = Client.EVMTokenAmount({
@@ -105,5 +115,27 @@ contract MultichainControl is OwnerIsCreator {
             address(0),
             fees
         );
+    }
+
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal override {
+
+        uint256 amount = message.destTokenAmounts[0].amount;
+
+        require(lockedTokens[_owner] >= amount, "Not enough locked tokens");
+        lockedTokens[_owner] -= amount;
+
+        IERC20(sourceTokenAddress).approve(address(this), amount);
+
+        (bool success, ) = address(sourceTokenAddress).call(message.data);
+        require(success, "Token transfer failed"); 
+   
+        emit TokensUnlocked(amount);
+    }
+
+    
+    function setSourceAddress(address _sourceTokenAddress) external onlyOwner {
+        sourceTokenAddress = IERC20(_sourceTokenAddress);
     }
 }
